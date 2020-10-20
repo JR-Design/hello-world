bfdec24617143f774a7f52115e1391732c91fa56
JR-Design <47480825+JR-Design@users.noreply.github.com>
2020-10-19 20:45:09 -0400

    Animation Update
    Animation for the landing page

--- js/main.js
+++ js/main.js
@@ -147,8 +147,22 @@ document.addEventListener("click", closeAllSelect);
 
 
 
+
+
 //INTRO ANIMATION JAZZ
+
+
+
+
 const intro = gsap.timeline({ defaults: { ease: "power1.Out" } });
+
+intro.to("nav", {translateY: -1000})
+intro.to(".highlight", {scaleX: 0})
+intro.to("#portraitTrim", {scaleY: 0, scaleX: 0})
+
+intro.to(".socialMedia > div > a > img", {scaleY: 1, translateX: "-100vw"})
+intro.to("#colorSelector", {scaleY: 1, translateX: -1000})
+intro.to("#portrait", { translateX: "100vw"})
 
 intro.to(".text", { y: "0%", duration: 1, stagger: 0.35 });
 intro.to(".slider2", { y: "-100%", duration: 2, delay: 0.5 });
@@ -156,4 +170,51 @@ intro.to(".intro", { y: "-100%", duration: 0.5 }, "-=1.5");
 intro.fromTo("main", { opacity: 1 }, { opacity: 1, duration: 1 }, "-=1");
 
 
+intro.to("nav", {scaleY: 1, translateY: 0, duration: 1, delay: 0.5, }, "-=1");
+intro.to(".hide2 span", {y:"0%", duration:1, delay: 0.35, stagger: 0.35 });
+intro.to(".hide2", {background: "none", duration:0.1});
+
+intro.to(".highlight", {scaleX: 1, duration: 0.45, delay: 0, transformOrigin: "bottom left"}, "-=0.2");
+intro.to("#portrait", {translateX: 0, duration: 0.7, delay: 0.25, });
+intro.to("#portraitTrim", {scaleY: 1, scaleX: 1, duration: 1,  }, "-=0.2");
+
+intro.to(".socialMedia > div > a > img", {scaleY: 1, translateX: 0, duration: 0.5, stagger: 0.05 }, "-=0.2");
+intro.to("#colorSelector", {scaleY: 1, translateX: 0, duration: 0.5, delay: 0, stagger: 0.25 });
+
+
+
+
+
+
+/*
+const currentLoad = localStorage.getItem("loaded");
+
+
+function introAnimation(){
+  if (currentLoad === null) {
+    localStorage.setItem("loaded", "true");
+    console.log("Runs");
+
+
+
+intro.to(".text", { y: "0%", duration: 1, stagger: 0.35 });
+intro.to(".slider2", { y: "-100%", duration: 2, delay: 0.5 });
+intro.to(".intro", { y: "-100%", duration: 0.5 }, "-=1.5");
+intro.fromTo("main", { opacity: 1 }, { opacity: 1, duration: 1 }, "-=1");
+    
+  } else {
+    localStorage.setItem("loaded", "false");
+    console.log("Doesn't Run")
+
+    intro.to(".text", { y: "0%", duration: 0.00001, stagger: 0 });
+    intro.to(".slider2", { y: "-100%", duration: 0.0001, delay: 0 });
+    intro.to(".intro", { y: "-100%", duration: 0.00001 }, "-=1.5");
+    intro.fromTo("main", { opacity: 1 }, { opacity: 1, duration: 1 }, "-=1");
+  }
+}
+
+
+  
+window.onload = introAnimation();
+*/
 
